a2e3253eb17ebb2409bdf7c91a3c973bba95e1d4
lau4225 <lauriebonnel@hotmail.com>
2018-11-28 15:23:33 -0500

    28 novembre 15h17 j'ai améliorer l'affichage du résultat,
    ce qui est réglé :
    les conditions d'erreurs pour + - hadam et multiScalaire
    (si la case est vide, si les mat sont incompletes)
    
    si les résultats 1,2,3,4 sont vides
    
    créée un autre dialogue pour quand on fait une opérations sur juste 1 matrice ou
    sur 2 matrices

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,7 @@
 package sample;
 
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
@@ -32,23 +32,46 @@ public class Controller {
 
         TextInputDialog ligneA = new TextInputDialog();
         ligneA.setTitle("Matrice A");
+        ligneA.setHeaderText("Création");
         ligneA.setContentText("Entrez le nombre de lignes");
         resultat1 = ligneA.showAndWait().get();
+        if (resultat1.equals("")){
+            ligneA.setHeaderText("Vous n'avez pas entré un nombre!");
+            resultat1 = ligneA.showAndWait().get();
+        }
 
         TextInputDialog colonneA = new TextInputDialog();
         colonneA.setTitle("Matrice A");
+        colonneA.setHeaderText("Création");
         colonneA.setContentText("Entrez le nombre de colonnes");
         resultat2 = colonneA.showAndWait().get();
+        if (resultat2.equals("")){
+            colonneA.setHeaderText("Vous n'avez pas entré un nombre!");
+            resultat2 = colonneA.showAndWait().get();
+        }
+
 
         TextInputDialog ligneB = new TextInputDialog();
         ligneB.setTitle("Matrice B");
+        ligneB.setHeaderText("Création");
         ligneB.setContentText("Entrez le nombre de lignes");
         resultat3 = ligneB.showAndWait().get();
+        if (resultat3.equals("")){
+            ligneB.setHeaderText("Vous n'avez pas entré un nombre!");
+            resultat3 = ligneB.showAndWait().get();
+        }
+
 
         TextInputDialog colonneB = new TextInputDialog();
         colonneB.setTitle("Matrice B");
+        colonneB.setHeaderText("Création");
         colonneB.setContentText("Entrez le nombre de colonnes");
         resultat4 = colonneB.showAndWait().get();
+        if (resultat4.equals("")){
+            colonneB.setHeaderText("Vous n'avez pas entré un nombre!");
+            resultat4 = colonneB.showAndWait().get();
+        }
+
 
         int nbTextA = Integer.parseInt(resultat1)*Integer.parseInt(resultat2);
         int nbTextB = Integer.parseInt(resultat3)*Integer.parseInt(resultat4);
@@ -78,19 +101,26 @@ public class Controller {
     Button somme;
     public void addition(){
 
+        boolean vide = false;
+
         if (resultat1.equals(resultat3) && resultat2.equals(resultat4)) {
             int nombre = Integer.parseInt(resultat1)*Integer.parseInt(resultat2);
             int[] resultat = new int[nombre];
 
-                for (int i = 0; i < nombre; i++) {
+            for (int i = 0; i < nombre; i++) { //regarder s'il y a des cases vides
+                if(tableau1[i].getText().isEmpty()|| tableau2[i].getText().isEmpty()){ //Si oui, vide est vrai
+                     vide = true;
+            }}
 
+            if (vide==true){ emptyDouble(); }
+            else {
+                for (int i = 0; i < nombre; i++) {
                     try {
                         resultat[i] = Integer.parseInt(tableau1[i].getText()) + Integer.parseInt(tableau2[i].getText());
-                    }catch (NumberFormatException e){
-                                                                                    //dialog à faire si aucun nombre d'entré dans une case
-                    }
-                }
+                    }catch (Exception e){ emptyDouble(); } }
                 reponse(resultat);
+            }
+
         }
         else {
              wrongDimensions();
@@ -100,43 +130,58 @@ public class Controller {
     @FXML
     Button difference;
     public void soustraction(){
+
+        boolean vide = false;
         if (resultat1.equals(resultat3) && resultat2.equals(resultat4)) {
-            //Si les dimensions sont pareils
             int nombre = Integer.parseInt(resultat1)*Integer.parseInt(resultat2);
             int[] resultat = new int[nombre];
 
-            for (int i = 0; i < nombre; i++) {
+            for (int i = 0; i < nombre; i++) { //regarder s'il y a des cases vides
+                if(tableau1[i].getText().isEmpty()|| tableau2[i].getText().isEmpty()){ //Si oui, vide est vrai
+                    vide = true;
+                }}
 
-                try {
-                    resultat[i] = Integer.parseInt(tableau1[i].getText()) - Integer.parseInt(tableau2[i].getText());
-                }catch (NumberFormatException e){
-                                                                        //dialog à faire si aucun nombre d'entré dans une case
-                }
+            if (vide==true){ emptyDouble(); }
+            else {
+                for (int i = 0; i < nombre; i++) {
+                    try {
+                        resultat[i] = Integer.parseInt(tableau1[i].getText()) - Integer.parseInt(tableau2[i].getText());
+                    }catch (Exception e){ emptyDouble(); } }
+                reponse(resultat);
             }
-            reponse(resultat);
+
         }
-        else { wrongDimensions(); }
+        else {
+            wrongDimensions();
+        }
     }
 
     @FXML
     Button hadam;
     public void hadamard(){
+        boolean vide = false;
         if (resultat1.equals(resultat3) && resultat2.equals(resultat4)) {
-            //Si les dimensions sont pareils
             int nombre = Integer.parseInt(resultat1)*Integer.parseInt(resultat2);
             int[] resultat = new int[nombre];
 
-            for (int i = 0; i < nombre; i++) {
+            for (int i = 0; i < nombre; i++) { //regarder s'il y a des cases vides
+                if(tableau1[i].getText().isEmpty()|| tableau2[i].getText().isEmpty()){ //Si oui, vide est vrai
+                    vide = true;
+                }}
 
-                try {
-                    resultat[i] = Integer.parseInt(tableau1[i].getText()) * Integer.parseInt(tableau2[i].getText());
-                }catch (NumberFormatException e){
-                    //dialog à faire si aucun nombre d'entré dans une case
-                }
+            if (vide==true){ emptyDouble(); }
+            else {
+                for (int i = 0; i < nombre; i++) {
+                    try {
+                        resultat[i] = Integer.parseInt(tableau1[i].getText()) * Integer.parseInt(tableau2[i].getText());
+                    }catch (Exception e){ emptyDouble(); } }
+                reponse(resultat);
             }
-            reponse(resultat);
+
         }
-        else { wrongDimensions(); }
+        else {
+            wrongDimensions();
+        }
     }
 
     @FXML
@@ -147,24 +192,55 @@ public class Controller {
     Button scalaireB;
     @FXML
     TextField produitNbB;
-    public void multiScalaire(){                    //probleme : affiche les 2 en même temps...  code pareil...
-        int[] corolaireA = new int[Integer.parseInt(resultat1)* Integer.parseInt(resultat2)];
+    public void multiScalaire() {
+        int[] corolaireA = new int[Integer.parseInt(resultat1) * Integer.parseInt(resultat2)];
+        boolean videA = false;
 
-        if (!produitNbA.getText().isEmpty()){ //Si la case n'est pas vide
-            for (int i = 0; i<corolaireA.length; i++){
-                corolaireA[i] = Integer.parseInt(tableau1[i].getText()) * Integer.parseInt(produitNbA.getText());
+        if (!produitNbA.getText().isEmpty()) { //Si la case n'est pas vide
+
+            for (int i = 0; i < corolaireA.length; i++) { //regarder s'il y a des cases vides
+                if(tableau1[i].getText().isEmpty()){ //Si oui, vide est vrai
+                    videA = true;
+                }}
+
+            if (videA){ emptySolo(); }
+            else {
+                for (int i = 0; i < corolaireA.length; i++) {
+                    try {
+                            corolaireA[i] = Integer.parseInt(tableau1[i].getText()) * Integer.parseInt(produitNbA.getText());
+                    } catch (Exception e) {
+                        emptySolo();
+                    }
+
+                }
+                solo(corolaireA, Integer.parseInt(resultat2));
+                produitNbA.setText("");
             }
-            solo(corolaireA, Integer.parseInt(resultat2));
-            produitNbA.setText("");
         }
 
-        int[] corolaireB = new int[Integer.parseInt(resultat3)* Integer.parseInt(resultat4)];
-        if (!produitNbB.getText().isEmpty()){ //Si la case n'est pas vide
-            for (int i = 0; i<corolaireB.length; i++){
-                corolaireB[i] = Integer.parseInt(tableau2[i].getText()) * Integer.parseInt(produitNbB.getText());
+
+        int[] corolaireB = new int[Integer.parseInt(resultat3) * Integer.parseInt(resultat4)];
+        boolean videB = false;
+        if (!produitNbB.getText().isEmpty()) { //Si la case n'est pas vide
+
+            for (int i = 0; i < corolaireB.length; i++) { //regarder s'il y a des cases vides
+                if(tableau2[i].getText().isEmpty()){ //Si oui, vide est vrai
+                    videB = true;
+                }}
+
+            if (videB){ emptySolo(); }
+            else {
+                for (int i = 0; i < corolaireB.length; i++) {
+                    try {
+                        corolaireB[i] = Integer.parseInt(tableau2[i].getText()) * Integer.parseInt(produitNbB.getText());
+                    } catch (Exception e) {
+                        emptySolo();
+                    }
+
+                }
+                solo(corolaireB, Integer.parseInt(resultat4));
+                produitNbB.setText("");
             }
-            solo(corolaireB, Integer.parseInt(resultat4));
-            produitNbB.setText("");
         }
     }
 
@@ -180,7 +256,7 @@ public class Controller {
         }else { wrongDimensions(); }
     }
 
-    public void produitVectoriel(){
+    public void produitVectoriel(){                                                        // rendu ici exceptions
 
         int[] resultat = new int[3];
 
@@ -225,7 +301,16 @@ public class Controller {
             reponse.add(new Label(String.valueOf(corolaire[i])), i%Integer.parseInt(resultat2), (int) (i/ Integer.parseInt(resultat2)));
         }
 
+        reponse.setAlignment(Pos.TOP_CENTER);
+        reponse.setVgap(10); reponse.setHgap(10);
+
+        for (Node r: reponse.getChildren()) {
+            r.setScaleX(1.2); r.setScaleY(1.2);
+        }
+
         Dialog dialog = new Dialog();
+        dialog.setHeight(500); dialog.setWidth(800);
+        dialog.setHeaderText("Matrice Résultante : ");
         dialog.getDialogPane().setContent(reponse);
         dialog.getDialogPane().getButtonTypes().add(
                 new ButtonType("Clear", ButtonBar.ButtonData.OK_DONE)
@@ -243,7 +328,17 @@ public class Controller {
             reponse.add(new Label(String.valueOf(corolaire[i])), i%colonnes, (int) (i/colonnes));
         }
 
+        reponse.setAlignment(Pos.TOP_CENTER);
+        reponse.setVgap(10); reponse.setHgap(10);
+
+        for (Node r: reponse.getChildren()) {
+            r.setScaleX(1.2); r.setScaleY(1.2);
+        }
+
+
         Dialog dialog = new Dialog();
+        dialog.setHeight(500); dialog.setWidth(800);
+        dialog.setHeaderText("Matrice Résultante : ");
         dialog.getDialogPane().setContent(reponse);
         dialog.getDialogPane().getButtonTypes().add(
                 new ButtonType("Clear", ButtonBar.ButtonData.OK_DONE)
@@ -253,7 +348,7 @@ public class Controller {
 
     }
 
-    public void wrongDimensions(){ //messafe d'erreur qui va afficher
+    public void wrongDimensions(){ //message d'erreur qui va afficher
 
         Alert alerte = new Alert(Alert.AlertType.INFORMATION);
         alerte.setTitle("Information importante");
@@ -262,5 +357,27 @@ public class Controller {
                 "Vous pouvez effectuer une autre opération ou bien cliquer sur démarrer");
         alerte.showAndWait();
     }
+
+    public void emptyDouble(){ //message d'erreur qui va afficher si toutes les cases ne sont pas remplies
+
+        Alert alerte = new Alert(Alert.AlertType.INFORMATION);
+        alerte.setTitle("Information importante");
+        alerte.setHeaderText("Vous devez remplir toutes les cases avant de pouvoir effectuer une opération!");
+        alerte.setContentText(
+                "Remplissez les matrices A et B avec des nombres entiers seulement! ");
+        alerte.showAndWait();
+    }
+
+    public void emptySolo(){ //message d'erreur qui va afficher si toutes les cases ne sont pas remplies
+
+        Alert alerte = new Alert(Alert.AlertType.INFORMATION);
+        alerte.setTitle("Information importante");
+        alerte.setHeaderText("Vous devez remplir toutes les cases avant de pouvoir effectuer une opération!");
+        alerte.setContentText(
+                "Remplissez la matrice avec des nombres entiers seulement! ");
+        alerte.showAndWait();
+    }
+
+
 
 }
